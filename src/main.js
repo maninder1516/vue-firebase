48cc40297c2b9bf29016548b5b4875c60a1972b7
Maninder Kumar <maninder1516@gmail.com>
2019-06-13 13:25:22 +0530

    Details taken from config files

--- src/main.js
+++ src/main.js
@@ -4,18 +4,20 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import env from '@/../env.json'
+
 Vue.config.productionTip = false;
 
 let app = '';
 
 // Initialize Firebase
 const config = {
-  apiKey: "AIzaSyB0rSrxKlolrHRHqFefwmMtTv_pmrkPhx0",
-  authDomain: "vue-firebase-tutorial-bf919.firebaseapp.com",
-  databaseURL: "https://vue-firebase-tutorial-bf919.firebaseio.com",
-  projectId: "vue-firebase-tutorial-bf919",
-  storageBucket: "vue-firebase-tutorial-bf919.appspot.com",
-  messagingSenderId: "710466499632"
+  apiKey: env.apiKey,
+  authDomain: env.authDomain,
+  databaseURL: env.databaseURL,
+  projectId: env.projectId,
+  storageBucket: env.storageBucket,
+  messagingSenderId: env.messagingSenderId
 };
 
 firebase.initializeApp(config);
